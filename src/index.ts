60565dc12478b1adea11faf4494abe7a67923bff
Emmanuel <140660538+AE4041@users.noreply.github.com>
2025-12-14 00:02:36 +0000

    refactor build script

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "./index.css"
 
-export { Avatar } from "@components/Avatar/Avatar"
-export { Button } from "@components/Button/Button"
-export { Message } from "@components/Message/Message"
-export { Tooltip, TooltipTrigger } from "@components/Tooltip/Tooltip"
+export { Avatar } from "./components/Avatar/Avatar"
+export { Button } from "./components/Button/Button"
+export { Message } from "./components/Message/Message"
+export { Tooltip, TooltipTrigger } from "./components/Tooltip/Tooltip"
